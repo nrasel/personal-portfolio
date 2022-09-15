3eaec9f7c4588316b9c844626a7f56d676a8efc2
nrasel <nrasel452@gmail.com>
2022-09-16 03:00:51 +0600

    dark theme

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/* =================== typing animation ============= */
+
+var typed = new Typed(".typing", {
+  strings: [
+    "",
+    "Web Designer",
+    "Web Developer",
+    "JavaScript Lover",
+    "Programmer",
+  ],
+  typeSpeed: 150,
+  BackSpeed: 60,
+  loop: true,
+});
