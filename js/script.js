efda9fcc927a2c6168d6b7bad33762f474e12cfc
nrasel <nrasel452@gmail.com>
2022-09-16 23:44:53 +0600

    toggle

--- js/script.js
+++ js/script.js
@@ -22,12 +22,11 @@ const nav = document.querySelector(".nav"),
 for (let i = 0; i < totalNavList; i++) {
   const a = navList[i].querySelector("a");
   a.addEventListener("click", function () {
-    for (let i = 0; i < totalSection; i++) {
-      allSection[i].classList.remove("back-section");
-    }
+    removeBackSection();
     for (let j = 0; j < totalNavList; j++) {
       if (navList[j].querySelector("a").classList.contains("active")) {
-        allSection[j].classList.add("back-section");
+        addBackSection(j);
+        // allSection[j].classList.add("back-section");
       }
       navList[j].querySelector("a").classList.remove("active");
     }
@@ -39,6 +38,16 @@ for (let i = 0; i < totalNavList; i++) {
   });
 }
 
+function removeBackSection() {
+  for (let i = 0; i < totalSection; i++) {
+    allSection[i].classList.remove("back-section");
+  }
+}
+
+function addBackSection(num) {
+  allSection[num].classList.add("back-section");
+}
+
 function showSection(element) {
   for (let i = 0; i < totalSection; i++) {
     allSection[i].classList.remove("active");
@@ -46,6 +55,28 @@ function showSection(element) {
   const target = element.getAttribute("href").split("#")[1];
   document.querySelector("#" + target).classList.add("active");
 }
+
+function updateNav(element) {
+  for (let i = 0; i < totalNavList; i++) {
+    navList[i].querySelector("a").classList.remove("active");
+    const target = element.getAttribute("href").split("#")[1];
+    if (
+      target ===
+      navList[i].querySelector("a").getAttribute("href").split("#")[1]
+    ) {
+      navList[i].querySelector("a").classList.add("active");
+    }
+  }
+}
+
+document.querySelector(".hire-me").addEventListener("click", function () {
+  const sectionIndex = this.getAttribute("data-section-index");
+
+  showSection(this);
+  updateNav(this);
+  removeBackSection();
+  addBackSection(sectionIndex);
+});
 
 const navTogglerBtn = document.querySelector(".nav-toggler"),
   aside = document.querySelector(".aside");
